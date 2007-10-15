305a03549d90f9052c912912aa9ea33e242d08d9
Robert M. Fuhrer <rmfuhrer@optonline.net>
2007-10-15 17:36:34 +0000

    Renamed com.ibm.watson.smapi* to org.eclipse.imp.smapi*

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 scp plugins/*.jar orquesta:/var/www/localhost/htdocs/eclipse/updates/SAFARI/plugins
-scp com.ibm.watson.safari.feature_0.1.1.jar orquesta:/var/www/localhost/htdocs/eclipse/updates/SAFARI/features
+scp org.eclipse.imp.feature_0.1.1.jar orquesta:/var/www/localhost/htdocs/eclipse/updates/SAFARI/features
